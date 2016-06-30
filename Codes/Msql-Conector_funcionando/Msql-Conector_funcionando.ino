69123bd6b09cf731d32da0f11b1a0352360b1621
Mr.Viana <rafaelgov95@gmail.com>
2016-06-30 03:10:56 -0400

    up

--- Codes/Msql-Conector_funcionando/Msql-Conector_funcionando.ino
+++ Codes/Msql-Conector_funcionando/Msql-Conector_funcionando.ino
@@ -19,7 +19,7 @@ DallasTemperature sensors(&oneWire);
 
 //sensor de tempertaura de ambiente
 DeviceAddress sensor1;
-int sensorAmbi =0;
+int sensorAmbi = 0;
 int sensorLuz = 0;
 int sensorTemp = 0;
 char sqlbuf[128];
@@ -37,41 +37,16 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Connecting...");
   dht.begin();
-//sensor de agua
+  //sensor de agua
   sensors.begin();
   Serial.print(sensors.getDeviceCount(), DEC);
   sensors.getAddress(sensor1, 0) ? Serial.println("TRUE") : Serial.println("FALSE");
- if (my_conn.mysql_connect(server_addr, 3306, user, password)) {
-     
- Serial.println("Successful reconnect!");
-
-    } else {
- Serial.println("ERRO AO CONECTAR !");
-
-    }
 }
 
-boolean conect() {
-  delay(2500);
-  if (my_conn.is_connected()) {
-    lerSensores();
-  } else {
-    my_conn.disconnect();
-    Serial.println("Connecting...");
-    if (my_conn.mysql_connect(server_addr, 3306, user, password)) {
-      delay(2500);
-      Serial.println("Successful reconnect!");
-      lerSensores();
-    } else {
-      Serial.println("Reconnect!");
-      conect();
-    }
-  }
-}
 void lerSensores() {
   // Le a informacao do sensor
   delay(5000);
-  sensorAmbi= (int)dht.readTemperature();
+  sensorAmbi = (int)dht.readTemperature();
   sensors.requestTemperatures();
   sensorTemp = (int)sensors.getTempC(sensor1);
   sensorLuz = luz();
@@ -89,13 +64,28 @@ int luz() {
 }
 void UpdateMsql() {
   delay(140000);
-  sprintf(sqlbuf, "INSERT INTO Hidroponia.sensores(luzSensor,tempSensor,tempAmbi,date) values ('%d','%d','%d',now())", sensorLuz, sensorTemp,sensorAmbi) ;
+  sprintf(sqlbuf, "INSERT INTO Hidroponia.sensores(luzSensor,tempSensor,tempAmbi,date) values ('%d','%d','%d',now())", sensorLuz, sensorTemp, sensorAmbi) ;
   my_conn.cmd_query(sqlbuf);
   Serial.println("Salvo!!");
   delay(140000);
 }
 void loop() {
-  delay(10000);
-  conect();
+  delay(5000);
+  if (my_conn.is_connected()) {
+     delay(20000);
+    lerSensores();
+    delay(100000);
+  } else {
+    my_conn.disconnect();
+    Serial.println("Connecting...");
+    if (my_conn.mysql_connect(server_addr, 3306, user, password)) {
+      delay(2500);
+      Serial.println("Successful reconnect!");
+      lerSensores();
+    } else {
+      Serial.println("Reconnect!");
+
+    }
+  }
 }
 
